d8c476aa2f20978b26ac9702a65965c923f1c597
Ethan Hair <ethan@astropanda.studio>
2023-08-01 12:52:46 -0500

    Finished implementing last

---
+++ test/HotChocolateMiddlewareParserTests/Dummy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotChocolateMiddlewareParserTests
+{
+    public class Dummy
+    {
+        public Dummy() { }
+
+        public string Property1 { get; set; }
+        public int Property2 { get; set; }
+        public decimal Property3 { get; set; }
+    }
+}
